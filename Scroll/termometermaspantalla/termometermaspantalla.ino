cc83ad91b303d26e8194ab93887a001db9080e0c
PabloSalinasM <pablo.salinas@usach.cl>
2017-11-27 12:06:59 -0300

    mostrando numeros

--- Scroll/termometermaspantalla/termometermaspantalla.ino
+++ Scroll/termometermaspantalla/termometermaspantalla.ino
@@ -13,7 +13,7 @@ int a;
 float t;
 int B=3975;                  //B value of the thermistor
 float resistance;
- 
+int contador = 0; 
 void setup() 
 { 
   lcd.begin(16, 2);
@@ -39,12 +39,62 @@ void loop()
   else if (t>= 30) pos = 180;
   else pos = 180;
   myservo.write(pos);
-  delay(1000);
-   Serial.println(t);
+  //delay(1000);
+  Serial.println(t);
     // delay at the end of the full loop:
-    delay(1000);
+    //delay(1000);
+  contador=+1;
+  if (contador == 14){
     lcd.clear();
-    String x = String(Serial.read());
-    lcd.print(t);
-    delay(1000);
+    contador=0;  
+    }
+    //String x = String(Serial.read());
+  byte myByte = Serial.read();
+  lcd.print( byteToStringDictionary(myByte) );
+  delay(50); 
  }
+
+  String byteToStringDictionary(int byteCode){
+    String traduction;
+    switch (byteCode) {
+      case 48:
+        traduction = "0";
+        break;
+      case 49:
+        traduction = "1";
+        break;
+      case 50:
+        traduction = "2";
+        break;
+      case 51:
+        traduction = "3";
+        break;
+      case 52:
+        traduction = "4";
+        break;
+      case 53:
+        traduction = "5";
+        break;
+      case 54:
+        traduction = "6";
+        break;
+      case 55:
+        traduction = "7";
+        break;
+      case 56:
+        traduction = "8";
+        break;
+      case 57:
+        traduction = "9";
+        break;
+      case 84:
+        traduction = "T";
+        break;
+      case 32:
+        traduction = " ";
+        break;
+      default:
+        traduction = "0";
+    }
+    return traduction;
+  }
--- src/main/java/com/iot/Arduino.java
+++ src/main/java/com/iot/Arduino.java
@@ -7,25 +7,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class Arduino extends AbstractSpringSerialPortConnector {
 
-    public String hol = "0";
-    public void processData(String line){
-        
-        if(line == null){
-            System.out.println("Line es NULL");
+    private String temperatura = "0";
+    private String tiempo = "00 00 00";
+    private String salida = "";
+    private String estadoAnterior = "";
+    private String [] entradaProcesada;
+    public void processData(String arduinoOut){
+        //arduinoOut = "100 00 20 00";
+
+        /*entradaProcesada = arduinoOut.split("\\s");
+        System.out.println(entradaProcesada[0]);
+        System.out.println(entradaProcesada[1]);
+        System.out.println(entradaProcesada[2]);
+        System.out.println(entradaProcesada[3]);*/
+        if(arduinoOut.equals(this.estadoAnterior)){
+           //System.out.println("Line es NULL");
         }else{
-            System.out.println("Line NO es NULL");
-            if(line.equals("1")){
-                System.out.println("estamos aqui 1");
+            //System.out.println("Line NO es NULL");
+            if(true){
+                //System.out.println("estamos aqui 1");
+
+                    int a = Integer.parseInt("1");
                 try {
-                    int a = Integer.parseInt(hol);
-                    a=a+1;
-                    String b = String.valueOf(a);
-                    sendMessage(b);
-                    hol = b;
-                    System.out.println("estamos aqui 2");
+                    sendMessage("T 012 00 24 00");
+                    System.out.println("conchetumaree");
                 } catch (IOException e) {
                     e.printStackTrace();
-                  }
+                }
+
+                System.out.println("estamos aqui 2");
+
             }
             else{
                 System.out.println("no entro al if");
